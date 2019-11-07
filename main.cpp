0d667c3db4d2f68d4e3ff9b3a0500ed9b90db960
matteov <matteo.volpi@epitech.eu>
2019-11-07 17:48:38 +0100

    [minor] move : move server folder in src directory + setup compilation for server

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
-#include "Logger.hpp"
-#include "Breakpoint.hpp"
+/*
+** EPITECH PROJECT, 2019
+** CPP_babel_2019
+** File description:
+** main
+*/
+
+#include "TcpNetwork.hpp"
+#include "UdpNetwork.hpp"
 
 int main()
 {
-	Debug::Logger *log = Debug::Logger::getInstance(Debug::STANDARD, Debug::Flags::fatal_off);
-	{
-		Debug::Breakpoint b("Logger class", "main", Debug::Flags::all_on);
-		log->generateDebugMessage(Debug::DEBUG, "Just a regular debug message", "main");
-		log->generateDebugMessage(Debug::INFO, "Info message", "main");
-		log->generateDebugMessage(Debug::WARNING, "Warning message, attention !", "main");
-		log->generateDebugMessage(Debug::ERROR, "Error", "main");
-		log->generateDebugMessage(Debug::FATAL, "Fatal message, everything is going haywire !", "main");
-	}
-	Debug::Breakpoint b1("unused breakpoint", "main", Debug::Flags::debug_off);
-	Debug::Breakpoint b2("Sleep", "main", Debug::Flags::all_on);
-	std::this_thread::sleep_for(std::chrono::seconds(2));
-	log->generateDebugMessage(Debug::DEBUG, "debug message", "test::writeDebugMessage");
-	return 0;
+    try {
+        boost::asio::io_context io;
+        Server::TcpNetwork serv(io);
+        // Server::UdpNetwork serv(io);
+        io.run();
+    } catch(const std::exception &e) {
+        ;
+    }
+    return 0;
 }
