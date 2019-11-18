bb9637ca30f60ea8b3a4ef805ff7d96c82594119
alang <arthur.lang@epitech.eu>
2019-11-18 20:44:33 +0100

    add boost_asio flag for linking and add an empty splash scene

---
+++ src/game/scenes/SplashScene.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** Rtype
+** File description:
+** splash screen
+*/
+
+#include "SplashScene.hpp"
+
+Scenes::SplashScene::SplashScene()
+{
+    std::cout << "Splash !" << std::endl;
+}
+
+Scenes::SplashScene::~SplashScene()
+{
+}
---
+++ src/game/scenes/SplashScene.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2019
+** Rtype
+** File description:
+** splash screen
+*/
+
+#pragma once
+
+#include <iostream>
+
+#include "IScene.hpp"
+
+namespace Scenes {
+
+    class SplashScene : public IScene {
+
+        public:
+            SplashScene();
+            ~SplashScene();
+
+        private:
+            /* data */
+    };
+}
