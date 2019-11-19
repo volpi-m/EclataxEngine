962d1082f54af9b339e9e188046730a37640629b
alang <arthur.lang@epitech.eu>
2019-11-19 15:32:48 +0100

    add abstract scenes and start Splash scene

---
+++ src/game/scenes/AScene.cpp
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2019
+** Rtype
+** File description:
+** AScene methods
+*/
+
+#include "AScene.hpp"
+
+Scenes::AScene::AScene(const std::string &name, std::shared_ptr<Module::EntityComponentSystem> &ECS) : _name(name), _ECS(ECS), _pop(true), _swap(false) {}
+
+Scenes::AScene::AScene(const char *name, std::shared_ptr<Module::EntityComponentSystem> &ECS) : _name(name), _ECS(ECS), _pop(true), _swap(false) {}
+
+Scenes::AScene::AScene(const std::string &name) : _name(name), _pop(true), _swap(false) {}
+
+Scenes::AScene::AScene(const char *name) : _name(name), _pop(true), _swap(false) {}
+
+std::string Scenes::AScene::name() const
+{
+    return _name;
+}
+
+void Scenes::AScene::setName(const std::string &name)
+{
+    _name = name;
+}
+
+bool Scenes::AScene::isToPop() const
+{
+    return _pop;
+}
+
+bool Scenes::AScene::isToSwap() const
+{
+    return _swap;
+}
+
+void Scenes::AScene::setECSInstance(std::shared_ptr<Module::EntityComponentSystem> &ECS)
+{
+    _ECS = ECS;
+}
