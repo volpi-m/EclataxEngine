c0a0bcaeabeb85b0cc5257252ad10b4ea3482e00
Lucas <lucas.tabis@epitech.eu>
2019-11-21 17:05:55 +0100

    [feat] Components : Added a script to generate components template automaticly

---
+++ create_component.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+display_help()
+{
+    echo "Wrong set of arguments, try with the following :"
+    echo "bash create_components.sh nameOfTheComponent"
+}
+
+if [[ "$#" -ne 1 ]]
+then
+    display_help
+    exit 84
+fi
+
+create_template()
+{
+    INPUT="template"
+
+    touch src/engine/ecs/components/$1Component.hpp
+    while IFS= read -r LINE
+    do
+	echo "${LINE//TEMPLATE/$1}" >> src/engine/ecs/components/$1Component.hpp
+    done < "$INPUT"
+}
+
+create_template $1
