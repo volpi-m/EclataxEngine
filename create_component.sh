5418c74fe1c6b51cbffa611477dee9cb2fbf4d1b
Lucas <lucas.tabis@epitech.eu>
2019-11-21 17:12:25 +0100

    [feat] Components : Updated script by adding a template test file.

--- create_component.sh
+++ create_component.sh
@@ -17,9 +17,19 @@ create_template()
     INPUT="template"
 
     touch src/engine/ecs/components/$1Component.hpp
+
     while IFS= read -r LINE
     do
 	echo "${LINE//TEMPLATE/$1}" >> src/engine/ecs/components/$1Component.hpp
+    done < "$INPUT"
+
+    INPUT="template_test"
+    
+    touch tests/component/test_$1.cpp
+
+    while IFS= read -r LINE
+    do
+	echo "${LINE//TEMPLATE/$1}" >> tests/component/test_$1.cpp
     done < "$INPUT"
 }
 
