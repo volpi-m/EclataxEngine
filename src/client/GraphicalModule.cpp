1db731f00bbb23da86dcee8ae3b35187d9bce260
Lucas <lucas.tabis@epitech.eu>
2019-11-28 20:58:03 +0100

    [merge] dev : merging graphical module.

--- src/client/GraphicalModule.cpp
+++ src/client/GraphicalModule.cpp
@@ -11,21 +11,61 @@
 
 #include "GraphicalModule.hpp"
 
-Client::GraphicalModule::GraphicalModule() : _window(sf::RenderWindow(sf::VideoMode(1920, 1080), "SFML works!")), _closed(false) {}
+Client::GraphicalModule::GraphicalModule() : _window(sf::RenderWindow(sf::VideoMode(1920, 1080), "SFML works!")), _closed(false)
+{
+    sf::Texture artefact;
+
+    artefact.create(50, 50);
+    _textures.emplace(0, std::make_pair("artefact", artefact));
+}
 
 sf::RenderWindow &Client::GraphicalModule::window()
 {
     return _window;
 }
 
-void Client::GraphicalModule::createEntity(unsigned long long id, sf::IntRect &rect)
+void Client::GraphicalModule::createEntity(std::size_t id)
 {
-    _entities.emplace(id, Entity(rect));
+    if (_entities.find(id) != _entities.end())
+        return;
+
+    std::shared_ptr<Client::Entity> entity(new Client::Entity(_textures[0].second));
+
+    _entities.emplace(id, entity);
 }
 
-void Client::GraphicalModule::createEntity(unsigned long long id, const std::string &filepath, sf::IntRect &rect)
+void Client::GraphicalModule::createEntity(std::size_t id, std::size_t txtId)
 {
-    _entities.emplace(id, Entity(filepath, rect));
+    if (_entities.find(id) != _entities.end()) {
+        if (_entities[id]->textureIdx() != txtId)
+            _entities[id]->setTextureIdx(txtId, _textures[txtId].second);
+        return;
+    }
+
+    std::shared_ptr<Client::Entity> entity(new Client::Entity(_textures[txtId].second, txtId));
+
+    _entities.emplace(id, entity);
+}
+
+std::size_t Client::GraphicalModule::addTexture(const std::string &filepath)
+{
+    // Checking if the texture already exists
+    for (auto &it : _textures)
+        if (it.second.first == filepath)
+            return it.first;
+
+    // If not, adding it to our unordered map
+    sf::Texture newTexture;
+
+    // Checking if the texture can be loaded
+    if (!newTexture.loadFromFile(filepath)) {
+        Debug::Logger *log = Debug::Logger::getInstance();
+
+        log->generateDebugMessage(Debug::INFO, "Couldn't load a texture", "Client::GraphicalModule::addTexture");
+        return 0;
+    }
+    _textures.emplace(_textures.size() + 1, std::make_pair(filepath, newTexture));
+    return _textures.size();
 }
 
 bool Client::GraphicalModule::run()
@@ -37,6 +77,36 @@ bool Client::GraphicalModule::run()
         display();
     }
     return _closed;
+}
+
+void Client::GraphicalModule::parsePackets(void *packet)
+{
+    Network::headerUdp *packetHeader = static_cast<Network::headerUdp *>(packet);
+    Network::Entity *entity = nullptr;
+
+    // The server sent data about an entity
+    if (packetHeader->code == Network::SERVER_TICK)
+        entity = getEntityParams(packetHeader);
+    if (entity) {
+        std::size_t id = addTexture((char *)entity->texture);
+        createEntity(entity->id, id);
+        _entities[entity->id]->setPosition(entity->x, entity->y, entity->z);
+    }
+}
+
+Network::Entity *Client::GraphicalModule::getEntityParams(Network::headerUdp *packetHeader)
+{
+    // getting the length of the path of the texture
+    unsigned int len = Network::UDP_BUF_SIZE - (sizeof(unsigned long long) + sizeof(float) * 3);
+    Network::Entity *packetEntity = new Network::Entity;
+
+    // Copying all packet data into the structure
+    std::memcpy(&(packetEntity->id), (unsigned long long *)packetHeader->data, sizeof(unsigned long long));
+    std::memcpy(&(packetEntity->x), (float *)(packetHeader->data + sizeof(unsigned long long)), sizeof(float));
+    std::memcpy(&(packetEntity->y), (float *)(packetHeader->data + sizeof(unsigned long long) + sizeof(float)), sizeof(float));
+    std::memcpy(&(packetEntity->z), (float *)(packetHeader->data + sizeof(unsigned long long) + sizeof(float) * 2), sizeof(float));
+    std::memcpy(packetEntity->texture, packetHeader->data + sizeof(unsigned long long) + sizeof(float) * 3, len);
+    return packetEntity;
 }
 
 void Client::GraphicalModule::processEvents()
@@ -58,7 +128,7 @@ void Client::GraphicalModule::display()
 
     // Draw all entities
     for (auto &it : _entities)
-        _window.draw(it.second.sprite());
+        _window.draw(it.second->sprite());
     
     // Display all drawings
     _window.display();
