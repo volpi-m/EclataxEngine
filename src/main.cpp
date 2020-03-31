444a53022bba0109f4707ea66d59259282fc6108
alang <arthur.lang@epitech.eu>
2020-03-31 18:42:34 +0200

    clean cmake

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ *  @file     src/main.cpp
+ *  @author   Arthur Lang on the 2020-03-31
+ *  @date     2020-03-31
+ * 
+ *  project: EclataxEngine
+ * 
+ */
+
+int main(int argc, char const **argv)
+{
+    return 0;
+}
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,3 +1,12 @@
+/**
+ *  @file     tests/main.cpp
+ *  @author   Arthur Lang on the 2020-03-31
+ *  @date     2020-03-31
+ * 
+ *  project: EclataxEngine
+ * 
+ */
+
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv)
