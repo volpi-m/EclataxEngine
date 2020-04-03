cda2a5c50d4946fd475d69cfc0587b77235c3a25
Lucas <lucas.tabis@epitech.eu>
2020-04-03 22:35:11 +0200

    :arrow_up: Deleted useless binary installation + added an example for the project manager.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,16 @@
  * 
  */
 
+#include "ECS.hpp"
+
 int main(int argc, char const **argv)
 {
+    Module::EntityComponentSystem ecs;
+
+    std::shared_ptr<ECS::Entity> entity = std::make_shared<ECS::Entity>("tag 1");
+    ecs.addEntity(entity);
+
+    std::cout << "Entity tag: " << ecs.entity(1)->tag() << std::endl;
+
     return 0;
 }
