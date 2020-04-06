194c3866745203e4857ec7d52a3bb9bb1bad8062
matteov <matteo.volpi@epitech.eu>
2020-04-06 22:45:52 +0200

    :arrow_up: provide a first simple implementation of LuaFunction

--- src/Lua/LuaFunction.cpp
+++ src/Lua/LuaFunction.cpp
@@ -1,38 +1,42 @@
 #include "LuaFunction.hpp"
 
-LuaFunction::LuaFunction(
-    const std::string &fileName,
-    const std::string &functionName,
-    std::size_t nbParam,
-    std::size_t nbReturn
-) : _fileName(fileName), _functionName(functionName), _nbParam(nbParam), _nbReturn(nbReturn)
+ECL::LuaFunction::LuaFunction(
+    const std::string &fileName, const std::string &funcName,
+    const std::string &paramTypes, const std::size_t nbReturn
+) : _fileName(fileName), _funcName(funcName), _paramTypes(paramTypes), _nbReturn(nbReturn), _currentArg(0)
 {
     _lua = luaL_newstate();
     if (!_lua)
         throw std::exception();
     luaL_openlibs(_lua);
 
-    if (loadFile(_fileName) != LUA_OK)
+    if (luaL_dofile(_lua, _fileName.c_str()) != LUA_OK)
         throw std::exception();
 }
 
-LuaFunction::~LuaFunction()
+ECL::LuaFunction::~LuaFunction()
 {
     lua_close(_lua);
 }
 
-bool LuaFunction::operator()()
+bool ECL::LuaFunction::operator()()
 {
-    lua_getglobal(_lua, _functionName.c_str());
+    Log &l = Log::get();
 
-    std::cout << _functionName << " " << _nbParam << " " << _nbReturn << std::endl;
+    if (_paramTypes.size() != 0)
+    {
+        l << Log::Color << Log::Error << Log::Time << "Bad argument number" << Log::Flush;
+        return false;
+    }
 
-    lua_call(_lua, _nbParam, _nbReturn);
+    lua_getglobal(_lua, _funcName.c_str());
+    if (!lua_isfunction(_lua, -1))
+    {
+        l << Log::Color << Log::Error << Log::Time << "The string specified is not a function" << Log::Flush;
+        return false;
+    }
+
+    lua_call(_lua, _paramTypes.size(), 0);
+
     return true;
-}
-
-int LuaFunction::loadFile(const std::string &name)
-{
-    _fileName = name;
-    return luaL_dofile(_lua, _fileName.c_str());
 }
--- src/Lua/LuaFunction.hpp
+++ src/Lua/LuaFunction.hpp
@@ -4,9 +4,8 @@
 #include <string>
 #include <iostream>
 #include <exception>
-#include <vector>
-#include <any>
-#include <cstring>
+
+#include "Log.hpp"
 
 extern "C"
 {
@@ -15,28 +14,71 @@ extern "C"
     #include <lauxlib.h>
 }
 
-class LuaFunction
+namespace ECL
 {
-public:
-    LuaFunction(const std::string &, const std::string &, std::size_t, std::size_t);
-    ~LuaFunction();
+    class LuaFunction
+    {
+    public:
+        LuaFunction(const std::string &, const std::string &, const std::string &, const std::size_t);
+        ~LuaFunction();
 
-    template<typename T, typename ... Args>
-    bool operator()(T first, Args ... args);
+        constexpr static auto const NONE = "";
+        constexpr static auto const INT = "i";
+        constexpr static auto const CSTRING = "s";
+        constexpr static auto const DOUBLE = "d";
+        constexpr static auto const BOOL = "b";
+/*
+        template<typename T, typename ...Args>
+        bool operator()(T first, Args... args)
+        {
+            Log &l = Log::get();
 
-    template<typename T>
-    bool operator()(T last);
+            if (_paramTypes.size() < 2)
+            {
+                l << Log::Color << Log::Error << Log::Time << "Bad argument number" << Log::Flush;
+                return false;
+            }
 
-    bool operator()();
+            pushOnStack(first);
 
-private:
-    int loadFile(const std::string &);
+            std::cout << "yeet" << std::endl;
+            return operator()(args...);
+        }
 
-    std::string _fileName;
-    std::string _functionName;
-    std::size_t _nbParam;
-    std::size_t _nbReturn;
-    lua_State *_lua;
-};
+        template<typename T>
+        bool operator()(T last)
+        {
+            Log &l = Log::get();
+
+            if (_paramTypes.size() < 1)
+            {
+                l << Log::Color << Log::Error << Log::Time << "Bad argument number" << Log::Flush;
+                return false;
+            }
+
+            pushOnStack(last);
+
+            std::cout << "ending" << std::endl;
+            return true;
+        }*/
+
+        bool operator()();
+
+    private:
+        template<typename T>
+        void pushOnStack(T arg)           { lua_pushinteger(_lua, arg); }
+        void pushOnStack(lua_Number arg)  { lua_pushnumber(_lua, arg); }
+        void pushOnStack(float arg)       { lua_pushnumber(_lua, arg); }
+        void pushOnStack(bool arg)        { lua_pushboolean(_lua, arg ? 1 : 0); }
+        void pushOnStack(const char *arg) { lua_pushstring(_lua, arg); }
+
+        const std::string _fileName;
+        const std::string _funcName;
+        const std::string _paramTypes;
+        const std::size_t _nbReturn;
+        std::size_t _currentArg;
+        lua_State *_lua;
+    };
+}
 
 #endif /* !LUAFUNCTION_HPP_ */
